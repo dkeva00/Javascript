b295374573756a16dc6a3cdaaad18663cdb5d10d
dkeva00 <reyeva00@gmail.com>
2022-04-10 22:11:33 -0500

    variables types

--- Leccion02/02-01-TiposDatos.js
+++ Leccion02/02-01-TiposDatos.js
@@ -5,3 +5,52 @@ console.log(nombre);
 //tipo int
 var numero = 5;
 console.log(numero);
+
+//tipo object
+var objeto = {
+    nombre : "Brisa",
+    apellido : "Quezada"
+}
+console.log(objeto);
+
+console.log(typeof objeto);
+console.log(typeof nombre); 
+
+//tipo boolean
+var bandera = false;
+console.log(bandera);
+
+//tipo function
+function miFuncion(){}
+console.log(typeof miFuncion);
+
+//tipo Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(simbolo);
+console.log(typeof simbolo); 
+
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+console.log(typeof Persona); 
+
+//tipo undefined
+var x;
+console.log(x);
+console.log(typeof x); 
+
+//tipo null = ausencia de valor
+var y = null;
+console.log(typeof y);
+
+//tipo array
+var autos = ['BMW','Audi','Mazda'];
+console.log(autos);
+console.log(typeof autos);
+
+//vacio
+var z = '';
+console.log(z);
---
+++ Leccion02/02-02-ConcatenarCadenas.js
@@ -0,0 +1,21 @@
+var nombre = 'Peke';
+var apellido = 'Ayanami';
+
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
+
+var nombreCompleto2 = 'Peke' + ' ' + 'Ayanami';
+console.log(nombreCompleto2);
+
+var x = nombre + 800;
+console.log(x);
+
+var y = nombre + 2 + 4;
+console.log(y);
+
+var z = nombre + (2 + 4);
+console.log(z);
+
+var z = 2 + 4 + nombre;
+console.log(z);
+
